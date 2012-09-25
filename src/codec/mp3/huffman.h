94e06910c4c702ab13574a5f3b69fc1f2357fbd4
giszo <giszo.k@gmail.com>
2012-09-25 17:00:19 +0200

    Added the missing tables to the huffman decoder and implemented linbits handling.

--- src/codec/mp3/huffman.h
+++ src/codec/mp3/huffman.h
@@ -9,7 +9,6 @@ class HuffmanDecoder
 	static void DecodeBigValueData(BitStream& bs, unsigned tableIndex, std::vector<int>& samples);
 	static void DecodeCount1Data(BitStream& bs, unsigned tableIndex, std::vector<int>& samples);
 
-    private:
 	struct HuffmanData
 	{
 	    // type of the huffman data (0=tree node, 1=real data)
@@ -24,6 +23,7 @@ class HuffmanDecoder
 	{
 	    // the actual data of the huffman table
 	    const HuffmanData* m_data;
+	    unsigned m_linbits;
 	};
 
 	struct HuffmanDataCount1
@@ -42,11 +42,24 @@ class HuffmanDecoder
 
 	static const HuffmanData s_table1[7];
 	static const HuffmanData s_table2[17];
+	static const HuffmanData s_table3[17];
+	static const HuffmanData s_table5[31];
+	static const HuffmanData s_table6[31];
+	static const HuffmanData s_table7[71];
+	static const HuffmanData s_table8[71];
+	static const HuffmanData s_table9[71];
+	static const HuffmanData s_table10[127];
+	static const HuffmanData s_table11[127];
+	static const HuffmanData s_table12[127];
+	static const HuffmanData s_table13[511];
+	static const HuffmanData s_table15[511];
+	static const HuffmanData s_table16[511];
+	static const HuffmanData s_table24[511];
 
 	static const HuffmanDataCount1 s_count1table0[31];
 	static const HuffmanDataCount1 s_count1table1[31];
 
-	static const unsigned s_bigValueHuffmanTableCount = 3; // for now ...
+	static const unsigned s_bigValueHuffmanTableCount = 32;
 	static const HuffmanTable s_bigValueHuffmanTables[s_bigValueHuffmanTableCount];
 
 	static const unsigned s_count1HuffmanTableCount = 2;
